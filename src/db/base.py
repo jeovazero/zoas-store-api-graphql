21c86532f01ab9853ac7cdf00868dafcfebbd94a
jeovazero <jeovapereira2000@live.com>
2019-04-24 01:29:32 -0300

    refactor(db-base): add environment variables

--- src/db/base.py
+++ src/db/base.py
@@ -1,9 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+import os
 
+DB = os.environ["POSTGRES_DB"]
+USER = os.environ["POSTGRES_USER"]
+PASSWORD = os.environ["POSTGRES_PASSWORD"]
+PORT = os.getenv("POSTGRES_PORT", 5432)
+HOST = os.getenv("POSTGRES_HOST", "localhost")
 
-engine = create_engine("postgresql://yornero:yornero@localhost:5432/zoas-store")
+engine = create_engine(f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}")
 Session = scoped_session(
     sessionmaker(bind=engine, autocommit=False, autoflush=False)
 )
