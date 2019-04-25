49b5bed79097abcac653cc5ebc5f127dac29aeb3
jeovazero <jeovapereira2000@live.com>
2019-04-25 02:46:47 -0300

    chore: update seed script

--- src/db/seed.py
+++ src/db/seed.py
@@ -1,5 +1,31 @@
-from .shirt import ShirtModel
+from .product import ProductModel
+from .photo import PhotoModel
 from .base import Session, engine, Base
+
+sticker = (
+    "https://res.cloudinary.com/sohdezoas"
+    "/image/upload/v1556136755/zoas/Group_14_mgkzzl.png"
+)
+book1 = (
+    "https://res.cloudinary.com/sohdezoas"
+    "/image/upload/v1556135859/zoas/Group_8_rfakg9.png"
+)
+book2 = (
+    "https://res.cloudinary.com/sohdezoas"
+    "/image/upload/v1556135845/zoas/Group_9_w0aim1.png"
+)
+mousepad11 = (
+    "https://res.cloudinary.com/sohdezoas"
+    "/image/upload/v1556136755/zoas/Group_11_egcx5s.png"
+)
+mousepad12 = (
+    "https://res.cloudinary.com/sohdezoas"
+    "/image/upload/v1556136756/zoas/Group_10_wntc1r.png"
+)
+mousepad2 = (
+    "https://res.cloudinary.com/sohdezoas"
+    "/image/upload/v1556136763/zoas/Group_13_viqda0.png"
+)
 
 
 def seed():
@@ -7,13 +33,49 @@ def seed():
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
-    shirts = [
-        ShirtModel("Blue", 34.00),
-        ShirtModel("Red", 92.00),
-        ShirtModel("Green", 104.00),
-        ShirtModel("Yellow", 45.99),
+    photosSticker = [PhotoModel(sticker)]
+    photosBook = [PhotoModel(book1), PhotoModel(book2)]
+    photosMousepad1 = [PhotoModel(mousepad11), PhotoModel(mousepad12)]
+    photosMousepad2 = [PhotoModel(mousepad2)]
+
+    products = [
+        ProductModel(
+            title="Zoas Sticker",
+            price=2.00,
+            description="A sticker of zoas trademark",
+            avaliable=50,
+            avaliability=True,
+            photos=photosSticker,
+        ),
+        ProductModel(
+            title="Zoas Agenda",
+            price=12.88,
+            description="An agenda of zoas trademark",
+            avaliable=20,
+            avaliability=True,
+            photos=photosBook,
+        ),
+        ProductModel(
+            title="Zoas Mousepad Model 1",
+            price=30.75,
+            description="A Mousepad of zoas trademark",
+            avaliable=11,
+            avaliability=True,
+            photos=photosMousepad1,
+        ),
+        ProductModel(
+            title="Zoas Mousepad Model 2",
+            price=30.70,
+            description="A Mousepad of zoas trademark",
+            avaliable=9,
+            avaliability=True,
+            photos=photosMousepad2,
+        ),
     ]
 
-    for _shirt in shirts:
-        Session.add(_shirt)
+    for product in products:
+        Session.add(product)
+        for photo in product.photos:
+            Session.add(photo)
+
     Session.commit()
