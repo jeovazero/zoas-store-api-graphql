3aa5f11da36d2b5873f9b08b930e569f51913f96
jeovazero <jeovapereira2000@live.com>
2019-04-26 01:07:51 -0300

    test: put product to cart

--- tests/test_app.py
+++ tests/test_app.py
@@ -118,3 +118,33 @@ def test_mutation_delete_cart(client):
     [session_name, session_value] = get_session(resp2)
     assert len(session_value) == 0
     assert json["data"]["deleteCart"]["confirmation"] == "success"
+
+
+def test_mutation_put_product_cart(client):
+    resp1 = create_cart(client)
+    assert len(get_session(resp1)[1]) > 1
+
+    resp2 = client.post(
+        "/graphql",
+        json={
+            "query": """
+            mutation {
+                putProductToCart(payload: { productId: 2, quantity: 10 }){
+                    productId
+                    quantity
+                    price
+                    photos {
+                        urls
+                    }
+                }
+            }
+            """
+        },
+    )
+
+    json = resp2.get_json()
+    product_of_cart = json["data"]["putProductToCart"][0]
+    assert product_of_cart["productId"] == 2
+    assert product_of_cart["quantity"] == 10
+    assert product_of_cart["price"] == 12.88
+    assert len(product_of_cart["photos"]) == 2
