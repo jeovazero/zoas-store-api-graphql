ab21958bed7e6d54e480e0d0a5d373b111dea657
jeovazero <jeovapereira2000@live.com>
2019-04-26 00:07:39 -0300

    test: create cart

--- tests/test_app.py
+++ tests/test_app.py
@@ -70,3 +70,47 @@ def test_query_products_pagination(client):
 
     assert len(products["items"]) == 2
     assert products["hasMoreItems"] is True
+
+
+def create_cart(client):
+    return client.post(
+        "/graphql",
+        json={
+            "query": """
+            mutation {
+                createCart { confirmation }
+            }
+            """
+        },
+    )
+
+
+def test_mutation_create_cart(client):
+    resp = create_cart(client)
+    json = resp.get_json()
+    cookie = resp.headers["Set-Cookie"]
+    [session_name, session_value] = cookie.split(";")[0].split("=")
+
+    assert len(session_value) > 1
+    assert json["data"]["createCart"]["confirmation"] == "success"
+
+
+"""
+def test_mutation_delete_cart(client):
+    with client.session_transaction() as session:
+        session['u']='fake_cookie'
+
+    resp = client.post(
+        "/graphql",
+        json={
+            "query": ""
+            mutation {
+                deleteCart { confirmation }
+            }
+            ""
+        },
+    )
+
+    print(resp.headers['Set-Cookie'].split(';'))
+    assert len(resp.headers['Set-Cookie']) == 0
+"""
