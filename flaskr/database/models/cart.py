75dd02fdd0232c20a11eb7ed0f8b2f914f1c93e1
jeovazero <jeovapereira2000@live.com>
2019-04-27 03:20:20 -0300

    feat(manage-cart): add cart and product_cart models

---
+++ flaskr/database/models/cart.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from ..base import Base
+
+
+class CartModel(Base):
+    __tablename__ = "carts"
+
+    id = Column(String, primary_key=True)
+    products = relationship("ProductCartModel", backref="cart")
