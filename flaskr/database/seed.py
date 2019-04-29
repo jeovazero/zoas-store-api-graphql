d4757fc93cd38f34d949127aba0fa75cd4d3c4e8
jeovazero <jeovapereira2000@live.com>
2019-04-29 19:12:25 -0300

    chore: add more products to seed script

--- flaskr/database/seed.py
+++ flaskr/database/seed.py
@@ -1,30 +1,22 @@
 from .models import ProductModel, PhotoModel
 from .base import Session, engine, Base
+import json
+from os import path
 
-sticker = (
-    "https://res.cloudinary.com/sohdezoas"
-    "/image/upload/v1556136755/zoas/Group_14_mgkzzl.png"
-)
-book1 = (
-    "https://res.cloudinary.com/sohdezoas"
-    "/image/upload/v1556135859/zoas/Group_8_rfakg9.png"
-)
-book2 = (
-    "https://res.cloudinary.com/sohdezoas"
-    "/image/upload/v1556135845/zoas/Group_9_w0aim1.png"
-)
-mousepad11 = (
-    "https://res.cloudinary.com/sohdezoas"
-    "/image/upload/v1556136755/zoas/Group_11_egcx5s.png"
-)
-mousepad12 = (
-    "https://res.cloudinary.com/sohdezoas"
-    "/image/upload/v1556136756/zoas/Group_10_wntc1r.png"
-)
-mousepad2 = (
-    "https://res.cloudinary.com/sohdezoas"
-    "/image/upload/v1556136763/zoas/Group_13_viqda0.png"
-)
+SEED_FOLDER = path.dirname(path.abspath(__file__))
+data_filename = path.join(SEED_FOLDER, "seed.data.json")
+
+
+def to_product_model(product):
+    photos = [PhotoModel(url) for url in product["photos"]]
+    return ProductModel(
+        title=product["title"],
+        price=product["price"],
+        description=product["description"],
+        avaliable=product["avaliable"],
+        avaliability=product["avaliability"],
+        photos=photos,
+    )
 
 
 def seed():
@@ -32,45 +24,10 @@ def seed():
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
-    photosSticker = [PhotoModel(sticker)]
-    photosBook = [PhotoModel(book1), PhotoModel(book2)]
-    photosMousepad1 = [PhotoModel(mousepad11), PhotoModel(mousepad12)]
-    photosMousepad2 = [PhotoModel(mousepad2)]
+    with open(data_filename) as data:
+        products_data = json.load(data)
 
-    products = [
-        ProductModel(
-            title="Zoas Sticker",
-            price=2.00,
-            description="A sticker of zoas trademark",
-            avaliable=50,
-            avaliability=True,
-            photos=photosSticker,
-        ),
-        ProductModel(
-            title="Zoas Agenda",
-            price=12.88,
-            description="An agenda of zoas trademark",
-            avaliable=20,
-            avaliability=True,
-            photos=photosBook,
-        ),
-        ProductModel(
-            title="Zoas Mousepad Model 1",
-            price=30.75,
-            description="A Mousepad of zoas trademark",
-            avaliable=11,
-            avaliability=True,
-            photos=photosMousepad1,
-        ),
-        ProductModel(
-            title="Zoas Mousepad Model 2",
-            price=30.70,
-            description="A Mousepad of zoas trademark",
-            avaliable=9,
-            avaliability=True,
-            photos=photosMousepad2,
-        ),
-    ]
+    products = [to_product_model(p) for p in products_data]
 
     for product in products:
         Session.add(product)
