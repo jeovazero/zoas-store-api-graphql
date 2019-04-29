bf25ac4fab2aaff9935cb099b8fb2800b22fc128
jeovazero <jeovapereira2000@live.com>
2019-04-29 00:47:32 -0300

    test: update mutation pay cart

--- tests/helpers.py
+++ tests/helpers.py
@@ -107,36 +107,48 @@ def get_product(client, pid):
     )
 
 
+def unpack_dict(d):
+    s = []
+    for key, val in d.items():
+        r = (
+            ("{ " + unpack_dict(val) + " }")
+            if isinstance(d[key], dict)
+            else f'"{val}"'
+        )
+        s.append(f"{key}: {r}")
+    return ",".join(s)
+
+
 def pay_cart(client, payload):
-    return client.post(
-        "/graphql",
-        json={
-            "query": """
-            mutation {"""
-            f'payCart(payload: {payload}")'
-            """{
-                customer
-                address {
-                    city
-                    country
-                    zipcode
-                    street
-                    number
-                    district
-                }
-                totalPaid
-                productsPaid {
-                    id
-                    title
-                    description
-                    photos {
-                        url
-                    }
-                    price
-                    quantity
-                }
-              }
+    params = " { " + unpack_dict(payload) + " }"
+    mutation = (
+        """
+        mutation {"""
+        f"payCart(payload: {params} )"
+        """{
+            customer
+            address {
+                city
+                country
+                zipcode
+                street
+                number
+                district
             }
-            """
-        },
+            totalPaid
+            productsPaid {
+                productId
+                title
+                description
+                photos {
+                    url
+                }
+                price
+                quantity
+            }
+          }
+        }
+    """
     )
+
+    return client.post("/graphql", json={"query": mutation})
--- tests/test_mutation_pay_cart.py
+++ tests/test_mutation_pay_cart.py
@@ -13,17 +13,17 @@ john_right = {
         "district": "Disctrict 10",
     },
     "creditCard": {
-        "cardNumber": "534553523434534",
-        "expiration_date": "01/27",
+        "cardNumber": "5345535234345344",
+        "expirationDate": "01/27",
         "cvv": "123",
     },
 }
 
 # Fake user with wrong credicard number
-john_wrong = copy.copy(john_right)
+john_wrong = copy.deepcopy(john_right)
 john_wrong["creditCard"] = {
-    "cardNumber": "534553523434530",
-    "expiration_date": "01/27",
+    "cardNumber": "5345535234345340",
+    "expirationDate": "01/27",
     "cvv": "123",
 }
 
@@ -69,26 +69,27 @@ def test_lack_of_stock(client):
     resp_1 = create_cart(client)
     assert len(get_session(resp_1)[1]) > 1
 
-    put_product_cart(client, pid="2", qtd=10)
-    json_1 = put_product_cart(client, pid="1", qtd=20)
+    put_product_cart(client, pid="3", qtd=6)
+    resp_put_1 = put_product_cart(client, pid="4", qtd=5)
 
-    assert json_1["data"]["payCart"] is not None
+    json_1 = resp_put_1.get_json()
+    assert not (json_1["data"]["putProductToCart"] is None)
 
     # save the session
-    session_1 = client.session_transaction()
-    session_id_1 = session_1["u"]
-    print("Session", session_1, session_id_1)
+    session_id_1 = ""
+    with client.session_transaction() as session_1:
+        session_id_1 = session_1["u"]
 
     # Second cart
     resp_2 = create_cart(client)
     assert len(get_session(resp_2)[1]) > 1
-    # 11 (in cart 2) + 10 (in cart 1) > 20 (total of product 2)
-    put_product_cart(client, pid="2", qtd=11)
+    # 6 (in cart 2) + 6 (in cart 1) > 11 (total of product 3)
+    put_product_cart(client, pid="3", qtd=6)
     resp_pay_2 = pay_cart(client, payload=john_right)
     json_2 = resp_pay_2.get_json()
     data_2 = json_2["data"]["payCart"]
     assert data_2["customer"] == "John Armless"
-    assert data_2["totalPaid"] == 141.68
+    assert data_2["totalPaid"] == 184.5
 
     # Payment of cart 1
     with client.session_transaction() as session:
@@ -101,7 +102,7 @@ def test_lack_of_stock(client):
     assert json_1["errors"] is not None
     assert (
         json_1["errors"][0]["message"]
-        == 'The product "Zoas Agenda" has lack in the stock'
+        == 'The product "Zoas Mousepad Model 1" has lack in the stock'
     )
 
 
